fd482e54a45de256f2d403cd914a8573f830c507
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2014-09-17 08:32:08 +0200

    First version of killgemfoce: pools are extremely big (100k at 12) it segfault during the 13th step for no apparent reason (I guess it ran out of stack). Also added my version of interval_tree.

---
+++ alt_interval_tree.h
@@ -0,0 +1,30 @@
+#ifndef _ALT_INTERVAL_TREE_H
+#define _ALT_INTERVAL_TREE_H
+
+float max (float a, float b)
+{
+  return a>b ? a : b;
+}
+
+void tree_add_element(float* tree, int N, int place, float val)
+{
+	place+=N;
+	while (place!=1) {
+		tree[place]=max(val,tree[place]);
+		place/=2;
+	}
+	tree[1]=max(val,tree[1]);
+}
+
+float tree_read_max(float* tree, int N, int place)
+{
+	place+=N;
+	float result = tree[place];
+	while (place!=1) {
+		if (place%2==0) result=max(result,tree[place+1]);
+		place/=2;
+	}
+	return result;
+}
+
+#endif // _ALT_INTERVAL_TREE_H
