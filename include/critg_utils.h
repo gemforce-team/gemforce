34600d0df6bff5e23e2d634d5a2a4a62a218e416
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2014-12-21 21:57:05 +0100

    Splitted common crit functions in a separate library

---
+++ include/critg_utils.h
@@ -0,0 +1,135 @@
+#ifndef _CRITG_UTILS_H
+#define _CRITG_UTILS_H
+
+struct Gem_Y {
+	int grade;				//using short does NOT improve time/memory usage
+	float damage;
+	float crit;
+	struct Gem_Y* father;
+	struct Gem_Y* mother;
+};
+
+int int_max(int a, int b)
+{
+	if (a > b) return a;
+	else return b;
+}
+
+void gem_comb_eq(gem *p_gem1, gem *p_gem2, gem *p_gem_combined)
+{
+	p_gem_combined->grade = p_gem1->grade+1;
+	if (p_gem1->damage > p_gem2->damage) p_gem_combined->damage = 0.87*p_gem1->damage + 0.71*p_gem2->damage;
+	else p_gem_combined->damage = 0.87*p_gem2->damage + 0.71*p_gem1->damage;
+	if (p_gem1->crit > p_gem2->crit) p_gem_combined->crit = 0.88*p_gem1->crit + 0.5*p_gem2->crit;
+	else p_gem_combined->crit = 0.88*p_gem2->crit + 0.5*p_gem1->crit;
+}
+
+void gem_comb_d1(gem *p_gem1, gem *p_gem2, gem *p_gem_combined)     //bigger is always gem1
+{
+	p_gem_combined->grade = p_gem1->grade;
+	if (p_gem1->damage > p_gem2->damage) p_gem_combined->damage = 0.86*p_gem1->damage + 0.7*p_gem2->damage;
+	else p_gem_combined->damage = 0.86*p_gem2->damage + 0.7*p_gem1->damage;
+	if (p_gem1->crit > p_gem2->crit) p_gem_combined->crit = 0.88*p_gem1->crit + 0.44*p_gem2->crit;
+	else p_gem_combined->crit = 0.88*p_gem2->crit + 0.44*p_gem1->crit;
+}
+
+void gem_comb_gn(gem *p_gem1, gem *p_gem2, gem *p_gem_combined)
+{
+	p_gem_combined->grade = int_max(p_gem1->grade, p_gem2->grade);
+	if (p_gem1->damage > p_gem2->damage) p_gem_combined->damage = 0.85*p_gem1->damage + 0.69*p_gem2->damage;
+	else p_gem_combined->damage = 0.85*p_gem2->damage + 0.69*p_gem1->damage;
+	if (p_gem1->crit > p_gem2->crit) p_gem_combined->crit = 0.88*p_gem1->crit + 0.44*p_gem2->crit;
+	else p_gem_combined->crit = 0.88*p_gem2->crit + 0.44*p_gem1->crit;
+}
+
+void gem_combine (gem *p_gem1, gem *p_gem2, gem *p_gem_combined)
+{
+	p_gem_combined->father=p_gem1;
+	p_gem_combined->mother=p_gem2;
+	int delta = p_gem1->grade - p_gem2->grade;
+	switch (delta){
+		case 0:
+			gem_comb_eq(p_gem1, p_gem2, p_gem_combined);
+			break;
+		case 1:
+			gem_comb_d1(p_gem1, p_gem2, p_gem_combined);
+			break;
+		case -1:
+			gem_comb_d1(p_gem2, p_gem1, p_gem_combined);
+			break;
+		default:
+			gem_comb_gn(p_gem1, p_gem2, p_gem_combined);
+			break;
+	}
+	if (p_gem_combined->damage < p_gem1->damage) p_gem_combined->damage = p_gem1->damage;
+	if (p_gem_combined->damage < p_gem2->damage) p_gem_combined->damage = p_gem2->damage;
+}
+
+void gem_init(gem *p_gem, int grd, double damage, double crit)
+{
+	p_gem->grade =grd;
+	p_gem->damage=damage;
+	p_gem->crit  =crit;
+	p_gem->father=NULL;
+	p_gem->mother=NULL;
+}
+
+int gem_less_equal(gem gem1, gem gem2)
+{
+	if (gem1.damage < gem2.damage) return 1;
+	else if (gem1.damage == gem2.damage && gem1.crit < gem2.crit) return 1;
+	else return 0;
+}
+
+void ins_sort (gem* gems, int len)
+{
+	int i,j;
+	gem element;
+	for (i=1; i<len; i++) {
+		element=gems[i];
+		for (j=i; j>0 && gem_less_equal(element, gems[j-1]); j--) {
+			gems[j]=gems[j-1];
+		}
+		gems[j]=element;
+	}
+}
+
+void quick_sort (gem* gems, int len)
+{
+	if (len > 20)  {
+		gem pivot = gems[len/2];
+		gem* beg = gems;
+		gem* end = gems+len-1;
+		while (beg <= end) {
+			while (gem_less_equal(*beg, pivot)) {
+				beg++;
+			}
+			while (gem_less_equal(pivot,*end)) {
+				end--;
+			}
+			if (beg <= end) {
+				gem temp = *beg;
+				*beg = *end;
+				*end = temp;
+				beg++;
+				end--;
+			}
+		}
+		if (end-gems+1 < gems-beg+len) {		// sort smaller first
+			quick_sort(gems, end-gems+1);
+			quick_sort(beg, gems-beg+len);
+		}
+		else {
+			quick_sort(beg, gems-beg+len);
+			quick_sort(gems, end-gems+1);
+		}
+	}
+}
+
+void gem_sort (gem* gems, int len)
+{
+	quick_sort (gems, len);		// partially sort
+	ins_sort (gems, len);			// finish the nearly sorted array
+}
+
+#endif // _CRITG_UTILS_H
