409807f21a5af2a52530accdf8ad0ad2fed54019
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-04-27 22:07:23 +0200

    Updated auto_setup.sh with new names and table organization

--- auto_setup.sh
+++ auto_setup.sh
@@ -12,31 +12,26 @@ mkdir bin
 
 if [ -d "managem" ]; then
 	echo "Compiling leech/managem programs"
-	gcc -O2 -lm -I "include" managem/leechquery.c   -o bin/leechquery
-	gcc -O2 -lm -I "include" managem/managemquery.c -o bin/managemquery
-	gcc -O2 -lm -I "include" managem/mgaquery.c     -o bin/mgaquery
-	gcc -O2 -lm -I "include" managem/mgomniaquery.c -o bin/mgomniaquery
+	gcc -O2 -lm -I "include" managem/leechquery.c    -o bin/leechquery
+	gcc -O2 -lm -I "include" managem/mgquery-alone.c -o bin/mgquery-alone
+	gcc -O2 -lm -I "include" managem/mgquery-amps.c  -o bin/mgquery-amps
+	gcc -O2 -lm -I "include" managem/mgquery-omnia.c -o bin/mgquery-omnia
 else
 	echo "No managem folder found, you get no leech/managem programs"
 fi
 
 if [ -d "killgem" ]; then
 	echo "Compiling killgem programs"
-	gcc -O2 -lm -I "include" killgem/killgemquery.c -o bin/killgemquery
-	gcc -O2 -lm -I "include" killgem/kgaquery.c     -o bin/kgaquery
-	gcc -O2 -lm -I "include" killgem/kgomniaquery.c -o bin/kgomniaquery
+	gcc -O2 -lm -I "include" killgem/kgquery-alone.c -o bin/kgquery-alone
+	gcc -O2 -lm -I "include" killgem/kgquery-amps.c  -o bin/kgquery-amps
+	gcc -O2 -lm -I "include" killgem/kgquery-omnia.c -o bin/kgquery-omnia
 else
 	echo "No killgem folder found, you get no killgem programs"
 fi
 
 if [ -d "gem_tables" ]; then
 	echo "Copying tables over"
-	cp gem_tables/table_leech  bin/table_leech
-	cp gem_tables/table_mgspec bin/table_mgspec
-	cp gem_tables/table_mgcomb bin/table_mgcomb
-	cp gem_tables/table_crit   bin/table_crit
-	cp gem_tables/table_kgspec bin/table_kgspec
-	cp gem_tables/table_kgcomb bin/table_kgcomb
+	cp -r gem_tables/ bin/gem_tables/
 else
 	echo "No gem_tables folder found, I hope you know what you are doing, or nothing will work"
 fi
