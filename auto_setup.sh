c28ca0fb41ec2a9eebb8f93ad592f06d63affd0b
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-05-04 01:15:19 +0200

    Updated auto_setup.sh with the new programs and switched to O3

--- auto_setup.sh
+++ auto_setup.sh
@@ -12,19 +12,21 @@ mkdir bin
 
 if [ -d "managem" ]; then
 	echo "Compiling leech/managem programs"
-	gcc -O2 -lm -I "include" managem/leechquery.c    -o bin/leechquery
-	gcc -O2 -lm -I "include" managem/mgquery-alone.c -o bin/mgquery-alone
-	gcc -O2 -lm -I "include" managem/mgquery-amps.c  -o bin/mgquery-amps
-	gcc -O2 -lm -I "include" managem/mgquery-omnia.c -o bin/mgquery-omnia
+	gcc -O3 -lm -I "include" managem/leechquery.c    -o bin/leechquery
+	gcc -O3 -lm -I "include" managem/mgquery-alone.c -o bin/mgquery-alone
+	gcc -O3 -lm -I "include" managem/mgquery-amps.c  -o bin/mgquery-amps
+	gcc -O3 -lm -I "include" managem/mgquery-setup.c -o bin/mgquery-setup
+	gcc -O3 -lm -I "include" managem/mgquery-omnia.c -o bin/mgquery-omnia
 else
 	echo "No managem folder found, you get no leech/managem programs"
 fi
 
 if [ -d "killgem" ]; then
 	echo "Compiling killgem programs"
-	gcc -O2 -lm -I "include" killgem/kgquery-alone.c -o bin/kgquery-alone
-	gcc -O2 -lm -I "include" killgem/kgquery-amps.c  -o bin/kgquery-amps
-	gcc -O2 -lm -I "include" killgem/kgquery-omnia.c -o bin/kgquery-omnia
+	gcc -O3 -lm -I "include" killgem/kgquery-alone.c -o bin/kgquery-alone
+	gcc -O3 -lm -I "include" killgem/kgquery-amps.c  -o bin/kgquery-amps
+	gcc -O3 -lm -I "include" killgem/kgquery-setup.c -o bin/kgquery-setup
+	gcc -O3 -lm -I "include" killgem/kgquery-omnia.c -o bin/kgquery-omnia
 else
 	echo "No killgem folder found, you get no killgem programs"
 fi
@@ -35,3 +37,5 @@ if [ -d "gem_tables" ]; then
 else
 	echo "No gem_tables folder found, I hope you know what you are doing, or nothing will work"
 fi
+
+echo "Setup completed, the programs can be found in the bin/ folder"
