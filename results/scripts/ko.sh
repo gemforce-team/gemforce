32839a29d56ab042e5818273f276a027146a5e1f
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-05-27 14:12:35 +0200

    Added new killgem omnia recipes up to 32

---
+++ results/scripts/ko.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+inf=$1
+sup=$2
+file=$3
+
+for ((i=inf; i<=sup; i*=2))
+do
+	j=$((i/2))
+	echo "$i - $j"
+	filename="ko"$(printf "%0.4d-%d" $i $j)
+	desc="$filename - N6 TC120 A60 - gemforce $(git describe --tags | cut -f1 -d "-") - $file"",table_crit"
+	if [ $i -le 128 ]
+	then
+		bin/kgquery-omnia -rqpte  -f "$file" "$i" "$j" | tail -n +51 > results/$filename".txt"
+		echo "$desc" >> results/$filename".txt"
+	else
+		bin/kgquery-omnia -rqpe  -f "$file" "$i" "$j" | tail -n +51 > results/$filename".txt"
+		echo "$desc" >> results/$filename".txt"
+	fi
+done
