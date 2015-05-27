b88ce8b0c3d429c27dd62c7d6d80415a76d08804
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-05-27 14:20:31 +0200

    Added "-std=c11" compilation flag in auto_setup

--- auto_setup.sh
+++ auto_setup.sh
@@ -3,6 +3,8 @@
 # this script should be called in the repo root
 # but I'll check all the folders anyway
 
+cflags="-O3 -lm -I ""include"" -std=gnu11"
+
 if [ ! -d "include" ]; then
 	echo "No include folder found, aborting"
 	exit 1
@@ -12,21 +14,21 @@ mkdir bin
 
 if [ -d "managem" ]; then
 	echo "Compiling leech/managem programs"
-	gcc -O3 -lm -I "include" managem/leechquery.c    -o bin/leechquery
-	gcc -O3 -lm -I "include" managem/mgquery-alone.c -o bin/mgquery-alone
-	gcc -O3 -lm -I "include" managem/mgquery-amps.c  -o bin/mgquery-amps
-	gcc -O3 -lm -I "include" managem/mgquery-setup.c -o bin/mgquery-setup
-	gcc -O3 -lm -I "include" managem/mgquery-omnia.c -o bin/mgquery-omnia
+	gcc $cflags managem/leechquery.c    -o bin/leechquery
+	gcc $cflags managem/mgquery-alone.c -o bin/mgquery-alone
+	gcc $cflags managem/mgquery-amps.c  -o bin/mgquery-amps
+	gcc $cflags managem/mgquery-setup.c -o bin/mgquery-setup
+	gcc $cflags managem/mgquery-omnia.c -o bin/mgquery-omnia
 else
 	echo "No managem folder found, you get no leech/managem programs"
 fi
 
 if [ -d "killgem" ]; then
 	echo "Compiling killgem programs"
-	gcc -O3 -lm -I "include" killgem/kgquery-alone.c -o bin/kgquery-alone
-	gcc -O3 -lm -I "include" killgem/kgquery-amps.c  -o bin/kgquery-amps
-	gcc -O3 -lm -I "include" killgem/kgquery-setup.c -o bin/kgquery-setup
-	gcc -O3 -lm -I "include" killgem/kgquery-omnia.c -o bin/kgquery-omnia
+	gcc $cflags killgem/kgquery-alone.c -o bin/kgquery-alone
+	gcc $cflags killgem/kgquery-amps.c  -o bin/kgquery-amps
+	gcc $cflags killgem/kgquery-setup.c -o bin/kgquery-setup
+	gcc $cflags killgem/kgquery-omnia.c -o bin/kgquery-omnia
 else
 	echo "No killgem folder found, you get no killgem programs"
 fi
