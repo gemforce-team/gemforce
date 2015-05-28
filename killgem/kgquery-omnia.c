gemforce-team/gemforce

commit 1d6261f9e764a682f31802ff9458aa9f03ebfd2f

Working cpair compressor for kquery-omnia

killgem/kgquery-omnia.c
@@ -9,6 +9,7 @@ typedef struct Gem_YB gem;
 typedef struct Gem_Y gemY;
 #include "crit_utils.h"
 #include "kga_utils.h"
+#include "cpair.h"
 #include "gfon.h"
 
 void worker(int len, int lenc, int output_options, char* filename, char* filenamec, char* filenameA, int TC, int As, int GT, int Namps)
@@ -87,7 +88,62 @@ void worker(int len, int lenc, int output_options, char* filename, char* filenam
 	int poolcf_length;
 	
 	KGCOMB_COMPRESSION
-	printf("Killgem comb compressed pool size:\t%d\n\n",poolcf_length);
+	printf("Killgem comb compressed pool size:\t%d\n",poolcf_length);
+
+	int cpairs_length;
+	cpair* cpairs;
+	
+	{
+		int length = poolcf_length*poolYc_length;
+		cpair* temp_array=malloc(length*sizeof(cpair));
+		int index=0;
+		for (int l=0; l<poolcf_length; ++l) {
+			for (int m=0; m<poolYc_length; ++m) {
+				double power = gem_power(poolcf[l]);
+				double rdmg  = poolYc[m].damage/poolcf[l].damage;
+				double rcrit = poolYc[m].crit / poolcf[l].crit;
+				temp_array[index++] = (cpair){power, rdmg, rcrit, poolcf+l, poolYc+m, 0};
+			}
+		}
+		cpair_sort_rcrit(temp_array,length);				/* work starts */
+		double lastrcrit=-1;
+		int tree_cell=0;
+		for (int l=0; l<length; ++l) {
+			if (temp_array[l].rcrit == lastrcrit) temp_array[l].place=tree_cell-1;
+			else {
+				temp_array[l].place=tree_cell++;
+				lastrcrit = temp_array[l].rcrit;
+			}
+		}
+		cpair_sort_xyz(temp_array,length);
+		int broken=0;
+		int tree_length= 1 << (int)ceil(log2(tree_cell));		/* this is pow(2, ceil()) bitwise */
+		double* tree=malloc((tree_length*2)*sizeof(double));
+		for (int l=0; l<tree_length*2; ++l) tree[l]=0;			/* init also tree[0], it's faster */
+		for (int l=length-1;l>=0;--l) {								/* start from large z */
+			cpair* p_cpair=temp_array+l;
+			if (dtree_check_after(tree, tree_length, p_cpair->place, p_cpair->rdmg)) {
+				dtree_add_element(tree, tree_length, p_cpair->place, p_cpair->rdmg);
+			}
+			else {
+				p_cpair->combg=NULL;
+				broken++;
+			}
+		}
+		free(tree);
+		
+		cpairs_length=length-broken;
+		cpairs=malloc(cpairs_length*sizeof(cpair));
+		index=0;
+		for (int j=0; j<length; ++j) {
+			if (temp_array[j].combg!=NULL) {
+				cpairs[index] = temp_array[j];
+				index++;
+			}
+		}
+		free(temp_array);
+	}
+	printf("Combine pairs pool size:\t%d\n\n",cpairs_length);
 
 	int j,k,h,l,m;							// let's choose the right gem-amp combo
 	gem gems[len];							// for every speccing value

include/cpair.h
@@ -1,0 +1,130 @@
+#ifndef _CPAIR_H
+#define _CPAIR_H
+
+typedef struct Cpair {
+	double power;
+	double rdmg;
+	double rcrit;
+	gem*   combg;
+	gemY*  comba;
+	int    place;
+} cpair;
+
+inline int cpair_less_xyz(cpair cpair1, cpair cpair2)
+{
+	if (cpair1.power != cpair2.power)
+		return cpair1.power<cpair2.power;
+	if (cpair1.rdmg != cpair2.rdmg)
+		return cpair1.rdmg<cpair2.rdmg;
+	return cpair1.rcrit<cpair2.rcrit;
+}
+
+void ins_sort_xyz (cpair* cpairs, int len)
+{
+	int i,j;
+	cpair element;
+	for (i=1; i<len; i++) {
+		element=cpairs[i];
+		for (j=i; j>0 && cpair_less_xyz(element, cpairs[j-1]); j--) {
+			cpairs[j]=cpairs[j-1];
+		}
+		cpairs[j]=element;
+	}
+}
+
+void quick_sort_xyz (cpair* cpairs, int len)
+{
+	if (len > 10)  {
+		cpair pivot = cpairs[len/2];
+		cpair* beg = cpairs;
+		cpair* end = cpairs+len-1;
+		while (beg <= end) {
+			while (cpair_less_xyz(*beg, pivot)) {
+				beg++;
+			}
+			while (cpair_less_xyz(pivot,*end)) {
+				end--;
+			}
+			if (beg <= end) {
+				cpair temp = *beg;
+				*beg = *end;
+				*end = temp;
+				beg++;
+				end--;
+			}
+		}
+		if (end-cpairs+1 < cpairs-beg+len) {		// sort smaller first
+			quick_sort_xyz(cpairs, end-cpairs+1);
+			quick_sort_xyz(beg, cpairs-beg+len);
+		}
+		else {
+			quick_sort_xyz(beg, cpairs-beg+len);
+			quick_sort_xyz(cpairs, end-cpairs+1);
+		}
+	}
+}
+
+void cpair_sort_xyz (cpair* cpairs, int len)
+{
+	quick_sort_xyz(cpairs, len);    // partially sort
+	ins_sort_xyz(cpairs, len);      // finish the nearly sorted array
+}
+
+inline int cpair_less_rcrit(cpair cpair1, cpair cpair2)
+{
+	return cpair1.rcrit<cpair2.rcrit;
+}
+
+void ins_sort_rcrit (cpair* cpairs, int len)
+{
+	int i,j;
+	cpair element;
+	for (i=1; i<len; i++) {
+		element=cpairs[i];
+		for (j=i; j>0 && cpair_less_rcrit(element, cpairs[j-1]); j--) {
+			cpairs[j]=cpairs[j-1];
+		}
+		cpairs[j]=element;
+	}
+}
+
+void quick_sort_rcrit (cpair* cpairs, int len)
+{
+	if (len > 10)  {
+		cpair pivot = cpairs[len/2];
+		cpair* beg = cpairs;
+		cpair* end = cpairs+len-1;
+		while (beg <= end) {
+			while (cpair_less_rcrit(*beg, pivot)) {
+				beg++;
+			}
+			while (cpair_less_rcrit(pivot,*end)) {
+				end--;
+			}
+			if (beg <= end) {
+				cpair temp = *beg;
+				*beg = *end;
+				*end = temp;
+				beg++;
+				end--;
+			}
+		}
+		if (end-cpairs+1 < cpairs-beg+len) {		// sort smaller first
+			quick_sort_rcrit(cpairs, end-cpairs+1);
+			quick_sort_rcrit(beg, cpairs-beg+len);
+		}
+		else {
+			quick_sort_rcrit(beg, cpairs-beg+len);
+			quick_sort_rcrit(cpairs, end-cpairs+1);
+		}
+	}
+}
+
+void cpair_sort_rcrit (cpair* cpairs, int len)
+{
+	quick_sort_rcrit (cpairs, len);    // partially sort
+	ins_sort_rcrit (cpairs, len);      // finish the nearly sorted array
+}
+
+
+#endif // _CPAIR_H

include/interval_tree.h
@@ -93,4 +93,30 @@ int ftree_check_after_debug(float* tree, int N, int place, float val)
 	return 1;					// if we never found bigger return 1
 }
 
+/* And also a double one */
+
+void dtree_add_element(double* tree, int N, int place, double val)
+{
+	place+=N;
+	while (place!=1) {
+		if (val > tree[place]) {
+			tree[place]=val;
+			place >>= 1;		// place/=2 bitwise, it's a bit faster
+		}
+		else return;			// no need to go on, they'll sure be even bigger
+	}
+	if (val > tree[1]) tree[1]=val;
+}
+
+int dtree_check_after(double* tree, int N, int place, double val)
+{
+	place+=N;
+	if (val <= tree[place]) return 0;
+	while (place!=1) {
+		if (place%2==0 && val <= tree[place+1]) return 0;
+		place >>= 1;			// place/=2 bitwise, it's a bit faster
+	}
+	return 1;					// if we never found bigger return 1
+}
+
 #endif // _INTERVAL_TREE_H

commit b8556c415d6cd7e90a0b3c884026ae2a15304316

Fixed memory leak introduced in 1d6261f (cpairs not freed)

killgem/kgquery-omnia.c
@@ -379,6 +379,7 @@ void worker(int len, int lenc, int output_options, char* filename, char* filenam
 	free(poolY);
 	free(poolY_length);
 	free(poolYf);
+	free(cpairs);
 	if (output_options & mask_red && len > 2) {
 		free(gem_array);
 	}