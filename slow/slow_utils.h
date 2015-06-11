6e9b15c5e53c34df1a481bc13593013d9f4fdcac
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-06-11 13:47:38 +0200

    Built slow suite analogous to leech one

---
+++ slow/slow_utils.h
@@ -0,0 +1,113 @@
+#ifndef _SLOW_UTILS_H
+#define _SLOW_UTILS_H
+
+struct Gem_O {
+	int grade;			//using short does NOT improve time/memory usage
+	double slow;		//using float does NOT improve time/memory usage
+	struct Gem_O* father;
+	struct Gem_O* mother;
+};
+
+#include "gem_utils.h"
+
+inline double slow_power(double slow)
+{
+	return 1 - pow(1 + slow*1.04/2, -0.37);
+}
+
+inline int gem_better(gem gem1, gem gem2)
+{
+	return gem1.slow>gem2.slow;
+}
+
+void gem_comb_eq(gem *p_gem1, gem *p_gem2, gem *p_gem_combined)
+{
+	p_gem_combined->grade = p_gem1->grade+1;
+	if (p_gem1->slow > p_gem2->slow) p_gem_combined->slow = 0.81*p_gem1->slow + 0.35*p_gem2->slow;
+	else p_gem_combined->slow = 0.81*p_gem2->slow + 0.35*p_gem1->slow;
+}
+
+void gem_comb_d1(gem *p_gem1, gem *p_gem2, gem *p_gem_combined)		//bigger is always gem1
+{
+	p_gem_combined->grade = p_gem1->grade;
+	if (p_gem1->slow > p_gem2->slow) p_gem_combined->slow = 0.8*p_gem1->slow + 0.28*p_gem2->slow;
+	else p_gem_combined->slow = 0.8*p_gem2->slow + 0.28*p_gem1->slow;
+}
+
+void gem_comb_gn(gem *p_gem1, gem *p_gem2, gem *p_gem_combined)
+{
+	p_gem_combined->grade = int_max(p_gem1->grade, p_gem2->grade);
+	if (p_gem1->slow > p_gem2->slow) p_gem_combined->slow = 0.79*p_gem1->slow + 0.26*p_gem2->slow;
+	else p_gem_combined->slow = 0.79*p_gem2->slow + 0.26*p_gem1->slow;
+}
+
+void gem_combine (gem *p_gem1, gem *p_gem2, gem *p_gem_combined)
+{
+	p_gem_combined->father=p_gem1;
+	p_gem_combined->mother=p_gem2;
+	int delta = p_gem1->grade - p_gem2->grade;
+	switch (delta){
+		case 0:
+			gem_comb_eq(p_gem1, p_gem2, p_gem_combined);
+			break;
+		case 1:
+			gem_comb_d1(p_gem1, p_gem2, p_gem_combined);
+			break;
+		case -1:
+			gem_comb_d1(p_gem2, p_gem1, p_gem_combined);
+			break;
+		default:
+			gem_comb_gn(p_gem1, p_gem2, p_gem_combined);
+			break;
+	}
+}
+
+void gem_init(gem *p_gem, int grd, double slow)
+{
+	p_gem->grade=grd;
+	p_gem->slow=slow;
+	p_gem->father=NULL;
+	p_gem->mother=NULL;
+}
+
+#include "red_adder.h"
+
+inline double gem_power(gem gem1) {
+	return gem1.slow;
+}
+
+gem* gem_putred(gem* pool, int pool_length, int value, gem* red, gem** gem_array)
+{
+	int isRed;
+	int last;
+	int curr;
+	double best_pow=0;
+	gem_init(red,1,0);
+	gem* best_gem=NULL;
+	gem* new_array;
+	gem* best_array=NULL;
+	int new_index;
+	int i;
+	for (i=0; i<pool_length; ++i) {
+		gem* gemf=pool+i;
+		for (last=0; last<value; last++) {
+			isRed=0;
+			curr=0;
+			new_array=malloc(value*sizeof(gem));
+			new_index=0;
+			gem* gp=gem_explore(gemf, &isRed, red, last, &curr, new_array, &new_index);
+			double new_pow=gem_power(*gp);
+			if (new_pow > best_pow) {
+				best_pow=new_pow;
+				free(best_array);
+				best_gem=gp;
+				best_array=new_array;
+			}
+			else free(new_array);
+		}
+	}
+	(*gem_array)=best_array;
+	return best_gem;
+}
+
+#endif // _SLOW_UTILS_H
---
+++ slow/slowquery.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <getopt.h>
+#include <string.h>
+typedef struct Gem_O gem;
+#include "slow_utils.h"
+#include "query_utils.h"
+#include "gfon.h"
+
+void gem_print(gem *p_gem) {
+	printf("Grade:\t%d\nSlow:\t%f\nSlow%%:\t%0.2f%%\n\n", p_gem->grade, p_gem->slow, slow_power(p_gem->slow)*100);
+}
+
+char gem_color(gem* p_gem) {
+	if (p_gem->slow==0) return 'r';
+	else return 'B';
+}
+
+#include "print_utils.h"
+
+void worker(int len, int output_options, char* filename)
+{
+	FILE* table=file_check(filename);      // file is open to read
+	if (table==NULL) exit(1);              // if the file is not good we exit
+	int i;
+	gem* gems=malloc(len*sizeof(gem));     // if not malloc-ed 230k is the limit
+	gem** pool=malloc(len*sizeof(gem*));   // if not malloc-ed 690k is the limit
+	int* pool_length=malloc(len*sizeof(int));    // if not malloced 400k is the limit (win)
+	pool[0]=malloc(sizeof(gem));
+	gem_init(gems,1,1);
+	gem_init(pool[0],1,1);
+	pool_length[0]=1;
+	
+	int prevmax=pool_from_table(pool, pool_length, len, table);		// pool filling
+	fclose(table);				// close
+	if (prevmax<len-1) {
+		for (i=0;i<=prevmax;++i) free(pool[i]);		// free
+		free(pool);				// free
+		free(pool_length);	// free
+		free(gems);				// free
+		if (prevmax>0) printf("Table stops at %d, not %d\n",prevmax+1,len);
+		exit(1);
+	}
+	
+	int skip_computations = (output_options & mask_quiet) && !((output_options & mask_table) || (output_options & mask_upto));
+	int first = skip_computations ? len-1 : 0;
+	for (i=first; i<len; ++i) {
+		gems[i]=pool[i][0];
+		for (int j=1; j<pool_length[i]; ++j) {
+			if (gem_better(pool[i][j],gems[i])) {
+				gems[i]=pool[i][j];
+			}
+		}
+		
+		if (!(output_options & mask_quiet)) {
+			printf("Value:\t%d\n",i+1);
+			if (output_options & mask_info)
+				printf("Growth:\t%f\n", log(gem_power(gems[i]))/log(i+1));
+			if (output_options & mask_debug)
+				printf("Pool:\t%d\n",pool_length[i]);
+			gem_print(gems+i);
+		}
+	}
+	
+	if (output_options & mask_quiet) {    // outputs last if we never seen any
+		printf("Value:\t%d\n",len);
+		printf("Growth:\t%f\n", log(gem_power(gems[len-1]))/log(len));
+		if (output_options & mask_debug)
+			printf("Pool:\t%d\n",pool_length[len-1]);
+		gem_print(gems+len-1);
+	}
+
+	gem* gemf=gems+len-1;  // gem that will be displayed
+
+	if (output_options & mask_upto) {
+		double best_growth=-INFINITY;
+		int best_index=0;
+		for (i=0; i<len; ++i) {
+			if (log(gem_power(gems[i]))/log(i+1) > best_growth) {
+				best_index=i;
+				best_growth=log(gem_power(gems[i]))/log(i+1);
+			}
+		}
+		printf("Best gem up to %d:\n\n", len);
+		printf("Value:\t%d\n",best_index+1);
+		printf("Growth:\t%f\n", best_growth);
+		gem_print(gems+best_index);
+		gemf = gems+best_index;
+	}
+
+	gem* gem_array;
+	gem red;
+	if (output_options & mask_red) {
+		if (len < 2) printf("I could not add red!\n\n");
+		else {
+			int value=gem_getvalue(gemf);
+			gemf = gem_putred(pool[value-1], pool_length[value-1], value, &red, &gem_array);
+			printf("Gem with red added:\n\n");
+			printf("Value:\t%d\n", value);    // made to work well with -u
+			printf("Growth:\t%f\n", log(gem_power(*gemf))/log(value));
+			gem_print(gemf);
+		}
+	}
+
+	if (output_options & mask_parens) {
+		printf("Compressed combining scheme:\n");
+		print_parens_compressed(gemf);
+		printf("\n\n");
+	}
+	if (output_options & mask_tree) {
+		printf("Gem tree:\n");
+		print_tree(gemf, "");
+		printf("\n");
+	}
+	if (output_options & mask_table) print_table(gems, len);
+	
+	if (output_options & mask_equations) {   // it ruins gems, must be last
+		printf("Equations:\n");
+		print_equations(gemf);
+		printf("\n");
+	}
+	
+	for (i=0;i<len;++i) free(pool[i]);		// free
+	free(pool);		// free
+	free(pool_length);
+	free(gems);		// free
+	if (output_options & mask_red && len > 1) {
+		free(gem_array);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	int len;
+	char opt;
+	int output_options=0;
+	char filename[256]="";		// it should be enough
+
+	while ((opt=getopt(argc,argv,"hptecidqurf:"))!=-1) {
+		switch(opt) {
+			case 'h':
+				print_help("hptecidqurf:");
+				return 0;
+			PTECIDQUR_OPTIONS_BLOCK
+			case 'f':
+				strcpy(filename,optarg);
+				break;
+			case '?':
+				return 1;
+			default:
+				break;
+		}
+	}
+	if (optind==argc) {
+		printf("No length specified\n");
+		return 1;
+	}
+	if (optind+1==argc) {
+		len = atoi(argv[optind]);
+	}
+	else {
+		printf("Too many arguments:\n");
+		while (argv[optind]!=NULL) {
+			printf("%s ", argv[optind]);
+			optind++;
+		}
+		printf("\n");
+		return 1;
+	}
+	if (len<1) {
+		printf("Improper gem number\n");
+		return 1;
+	}
+	file_selection(filename, "table_slow");
+	worker(len, output_options, filename);
+	return 0;
+}
+
