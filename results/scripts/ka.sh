3da2c42ece14c5a2075cd050ed7d7f6bfdcdc303
Andrea Stacchiotti <crudepatate@gmail.com>
2015-08-22 16:38:25 +0200

    Improved kg amps recipes with kssemi table

--- results/scripts/ka.sh
+++ results/scripts/ka.sh
@@ -7,13 +7,19 @@ for ((i=inf; i<=sup; i*=2))
 do
 	echo $i
 	filename="ka"$(printf "%0.4d" $i)
+	filenameu="ka"$(printf "%0.4d" $i)"u"
 	desc="$filename - N8 TC120 A60 - gemforce $(git describe --tags | cut -f1 -d "-") - $file"",table_crit"
+	descu="$filenameu - N8 TC120 A60 - gemforce $(git describe --tags | cut -f1 -d "-") - $file"",table_crit"
 	if [ $i -le 256 ]
 	then
-		bin/kgquery-amps -rqpte -f "$file" "$i" | tail -n +32 > results/$filename".txt"
+		bin/kgquery-amps -rqpte  -f "$file" "$i" | tail -n +32 > results/$filename".txt"
 		echo "$desc" >> results/$filename".txt"
+		bin/kgquery-amps -rqupte -f "$file" "$i" | tail -n +54 > results/$filenameu".txt"
+		echo "$descu" >> results/$filenameu".txt"
 	else
 		bin/kgquery-amps -rqpe  -f "$file" "$i" | tail -n +32 > results/$filename".txt"
 		echo "$desc" >> results/$filename".txt"
+		bin/kgquery-amps -rqupe -f "$file" "$i" | tail -n +54 > results/$filenameu".txt"
+		echo "$descu" >> results/$filenameu".txt"
 	fi
 done
